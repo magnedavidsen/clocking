ac98dfc96ec020942321c6430a8735005844fab5
Magne Davidsen <magne.davidsen@gmail.com>
2012-10-07 22:50:54 +0200

    add employees functioning. added some styles

--- src/clocking/views/common.clj
+++ src/clocking/views/common.clj
@@ -1,12 +1,14 @@
 (ns clocking.views.common
   (:use [noir.core :only [defpartial]]
-        [hiccup.page :only [include-css html5]]))
+        [hiccup.page :only [include-css include-js html5]]))
 
 (defpartial layout [& content]
             (html5
               [:head
                [:title "clocking"]
-               (include-css "/css/reset.css")]
+               (include-css "/css/reset.css")
+               [:link {:rel "stylesheet/less" :type "text/css" :href "/css/default.less"}]
+               (include-js "http://lesscss.googlecode.com/files/less-1.3.0.min.js")]
               [:body
                [:div#wrapper
                 content]]))
---
+++ src/clocking/views/employees.clj
@@ -0,0 +1,33 @@
+(ns clocking.views.employees
+  (:require [clocking.views.common :as common]
+            [clocking.db :as db])
+  (:use [noir.core]
+        [hiccup.form]
+        [hiccup.page]))
+
+(defpage "/employees" []
+  (common/layout
+   (add-employee-form)
+   (employees-table (db/list-all-employees))))
+
+(defpage [:post "/employees/add"] {:as employee}
+  (db/create-employee (Integer/parseInt  (:id employee)) (:name employee))
+  (render "/employees"))
+
+(defpartial add-employee-form []
+  (form-to {:autocomplete "off"} [:post "/employees/add"]
+           (label "id" "Employee ID: ")
+           (text-field {:class "employee-id"} "id")
+           (label "name" "Name: ")
+           (text-field "name")
+           (submit-button "Add employee")))
+
+(defpartial employees-table [employees]
+  [:table
+   [:tr
+    [:th "Employee ID"] [:th "Name"]]
+   (map employee-row employees)])
+
+(defpartial employee-row [{:keys [id name]}]
+  [:tr
+   [:td id] [:td name]])
