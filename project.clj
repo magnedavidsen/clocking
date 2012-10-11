62f5ce441e258b1d2e834aceef871832819d8451
Magne Davidsen <magne.davidsen@gmail.com>
2012-10-11 22:25:04 +0200

    asking heroku to use leiningen 2

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject clocking "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
+            :min-lein-version "2.0.0"
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [noir "1.3.0-beta3" :exclusions [org.clojure/clojure]]
                            [korma "0.3.0-beta9"]
