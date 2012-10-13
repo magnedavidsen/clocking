e5f9e580e14f2e7c9a1bb8824536f798926c2f50
Magne Davidsen <magne.davidsen@gmail.com>
2012-10-13 15:49:53 +0200

    trying to get jdbc to work on heroku

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [noir "1.3.0-beta3" :exclusions [org.clojure/clojure]]
                            [korma "0.3.0-beta9"]
-                           [postgresql "9.1-901.jdbc4"]
+                           [postgresql "9.1-901-1.jdbc4"]
                            [org.clojure/java.jdbc "0.1.1"]]
             :dev-dependencies [[org.clojure/java.jdbc "0.2.3"]
                                                 [postgresql "9.1-901.jdbc4"]]
