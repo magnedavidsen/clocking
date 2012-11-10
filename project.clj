d1b86650b3c1969b0933fb815fe4faec3642a3a1
Magne Davidsen <magne.davidsen@gmail.com>
2012-11-10 16:13:29 +0100

    jdbc version

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                            [noir "1.3.0-beta3" :exclusions [org.clojure/clojure]]
                            [korma "0.3.0-beta9"]
                            [postgresql "9.1-901-1.jdbc4"]
-                           [org.clojure/java.jdbc "0.2.3"]]
-            :dev-dependencies [[org.clojure/java.jdbc "0.2.3"]
+                           [org.clojure/java.jdbc "0.1.1"]]
+            :dev-dependencies [[org.clojure/java.jdbc "0.1.1"]
                                [postgresql "9.1-901-1.jdbc4"]]
             :main clocking.server)
