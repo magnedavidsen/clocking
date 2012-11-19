c09f3a058a1679634e9a9a056417c6ebc5628c01
Magne Davidsen <magne.davidsen@gmail.com>
2012-11-19 22:41:14 +0100

    styling and autofocus on input

--- resources/public/js/default.js
+++ resources/public/js/default.js
@@ -4,28 +4,31 @@ function setHiddenField(){
 }
 
 $(document).ready(function(){
-   $('.employee-id').keypress(function(event){
-   	console.log(event.keyCode);
 
-   	//if enter pressed, don't do anything
-    if (event.keyCode == 10 || event.keyCode == 13){
-    	event.preventDefault();
-    }
-        
-    //if 'i' is pressed, clock in
-    if (event.keyCode == 105){
-    	$('.clock-in').click();
-    }
-    
-    //if 'o' is pressed, clock out	
-    if (event.keyCode == 111){
-    	setHiddenField();
-    	$('.clock-out').click();
-    }
+	$('.employee-id').focus();
 
-    if($('.employee-id').val().length == 2){
-    	$('.help-text').html("(Press <b>'I</b> to clock in, <b>'O'</b> to clock out)");
-    }
+	$('.employee-id').keypress(function(event){
+
+	   	//if enter pressed, don't do anything
+	    if (event.keyCode == 10 || event.keyCode == 13){
+	    	event.preventDefault();
+	    }
+	        
+	    //if 'i' is pressed, clock in
+	    if (event.keyCode == 105){
+	    	$('.clock-in').click();
+	    }
+	    
+	    //if 'o' is pressed, clock out	
+	    if (event.keyCode == 111){
+	    	setHiddenField();
+	    	$('.clock-out').click();
+	    }
+
+	    if($('.employee-id').val().length == 2){
+	    	$('.help-text').html("(Press <b>'I</b> to clock in, <b>'O'</b> to clock out)");
+	    }
+
     	
   });
  });
