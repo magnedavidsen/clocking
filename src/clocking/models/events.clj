e0cc2180e2419380070b1890bea743f43513a3df
Magne Davidsen <magne.davidsen@gmail.com>
2013-01-20 14:30:19 +0100

    function to pair clockin/outs

--- src/clocking/models/events.clj
+++ src/clocking/models/events.clj
@@ -1,5 +1,14 @@
 (ns clocking.models.events)
 
-(defn pair-clockins-and-clockouts [events]
+(defn pair-clockins-and-clockouts [events paired-events]
+  (if (empty? events)
+    paired-events
+    (if (= "clock-out" (:type (first events)))
+      (pair-clockins-and-clockouts (rest events)
+                                   (conj paired-events (vector (first events))))
 
-)
+      (if (= "clock-in" (:type (first events)))
+        (do
+          (if (= "clock-out" (:type (second events)))
+            (pair-clockins-and-clockouts (rest (rest events)) (conj paired-events (vector (first events) (second events))))
+            (pair-clockins-and-clockouts (rest events) (conj paired-events (vector  (first events))))))))))
--- test/clocking/test/models/events.clj
+++ test/clocking/test/models/events.clj
@@ -2,8 +2,17 @@
   (:use [clojure.test]
         [clocking.models.events :as events]))
 
-(defonce event-pair [{:id 1, :type "clock-in", :time #inst "2012-10-16T23:15:22.214421000-00:00", :employee_id 100} {:id 2, :type "clock-out", :time #inst "2012-10-16T23:49:07.818952000-00:00", :employee_id 100}])
+(def event-list [
+                     {:id 1, :type "clock-out", :time #inst "2012-10-16T23:15:22.214421000-00:00", :employee_id 100}
+                     {:id 2, :type "clock-in", :time #inst "2012-10-16T23:49:07.818952000-00:00", :employee_id 100}
+                     {:id 3, :type "clock-in", :time #inst "2012-10-16T23:49:07.818952000-00:00", :employee_id 100}
+                     {:id 4, :type "clock-out", :time #inst "2012-10-16T23:49:07.818952000-00:00", :employee_id 100}])
 
-(defonce wanted-format {:employee_id 100, :clock-in-time #inst "2012-10-16T23:15:22.214421000-00:00", :clock-out-time  "2012-10-16T23:49:07.818952000-00:00"})
+(def wanted-format [
+                    [{:type "clock-out", :employee_id 100, :time #inst "2012-10-16T23:15:22.214-00:00", :id 1}]
+                    [{:type "clock-in", :employee_id 100, :time #inst "2012-10-16T23:49:07.818-00:00", :id 2}]
+                    [{:type "clock-in", :employee_id 100, :time #inst "2012-10-16T23:49:07.818-00:00", :id 3} {:type "clock-out", :employee_id 100, :time #inst "2012-10-16T23:49:07.818-00:00", :id 4}]])
 
-(events/pair-clockins-and-clockouts event-pair)
+(deftest first-test
+  (is (= wanted-format
+         (events/pair-clockins-and-clockouts event-list []))))
