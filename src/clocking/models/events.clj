e2ce3d073d4a59936f1cfe9647e82d7120855bf6
Magne Davidsen <magne.davidsen@gmail.com>
2013-01-26 13:21:00 +0100

    functions for flattening two events, with tests

--- src/clocking/models/events.clj
+++ src/clocking/models/events.clj
@@ -12,3 +12,13 @@
           (if (= "clock-out" (:type (second events)))
             (pair-clockins-and-clockouts (rest (rest events)) (conj paired-events (vector (first events) (second events))))
             (pair-clockins-and-clockouts (rest events) (conj paired-events (vector  (first events))))))))))
+
+(defn flatten-type-and-time [event]
+  {(keyword (:type event)) (:time event)})
+
+(defn flatten-two-events [events]
+  (let [flat-type-time (map flatten-type-and-time events)]
+    (merge
+     {:employee_id (:employee_id (first events)) }
+     (merge (first flat-type-time) (second flat-type-time)))))
+
--- test/clocking/test/models/events.clj
+++ test/clocking/test/models/events.clj
@@ -3,16 +3,37 @@
         [clocking.models.events :as events]))
 
 (def event-list [
-                     {:id 1, :type "clock-out", :time #inst "2012-10-16T23:15:22.214421000-00:00", :employee_id 100}
-                     {:id 2, :type "clock-in", :time #inst "2012-10-16T23:49:07.818952000-00:00", :employee_id 100}
-                     {:id 3, :type "clock-in", :time #inst "2012-10-16T23:49:07.818952000-00:00", :employee_id 100}
-                     {:id 4, :type "clock-out", :time #inst "2012-10-16T23:49:07.818952000-00:00", :employee_id 100}])
+                 {:id 1, :type "clock-out", :time #inst "2012-10-16T23:15:22.214421000-00:00", :employee_id 100}
+                 {:id 2, :type "clock-in", :time #inst "2012-10-16T23:49:07.818952000-00:00", :employee_id 100}
+                 {:id 3, :type "clock-in", :time #inst "2012-10-16T23:49:07.818952000-00:00", :employee_id 100}
+                 {:id 4, :type "clock-out", :time #inst "2012-10-16T23:49:07.818952000-00:00", :employee_id 100}])
 
 (def wanted-format [
                     [{:type "clock-out", :employee_id 100, :time #inst "2012-10-16T23:15:22.214-00:00", :id 1}]
                     [{:type "clock-in", :employee_id 100, :time #inst "2012-10-16T23:49:07.818-00:00", :id 2}]
                     [{:type "clock-in", :employee_id 100, :time #inst "2012-10-16T23:49:07.818-00:00", :id 3} {:type "clock-out", :employee_id 100, :time #inst "2012-10-16T23:49:07.818-00:00", :id 4}]])
 
-(deftest first-test
+(def one-event
+  [{:type "clock-in", :employee_id 100, :time #inst "2012-10-16T23:49:07.818-00:00", :id 3}])
+
+(def two-events
+  [{:type "clock-in", :employee_id 100, :time #inst "2012-10-16T23:49:07.818-00:00", :id 3}
+   {:type "clock-out", :employee_id 100, :time #inst "2012-10-16T23:49:07.818-00:00", :id 4}])
+
+(def one-event-flattened
+  {:employee_id 100 :clock-in #inst "2012-10-16T23:49:07.818-00:00"})
+
+(def two-events-flattened
+  {:employee_id 100 :clock-in #inst "2012-10-16T23:49:07.818-00:00" :clock-out #inst "2012-10-16T23:49:07.818-00:00"})
+
+(deftest events-get-paired-correctly
   (is (= wanted-format
          (events/pair-clockins-and-clockouts event-list []))))
+
+(deftest two-events-get-flattened-correctly
+  (is (= two-events-flattened
+         (events/flatten-two-events two-events))))
+
+(deftest one-event-get-flattened-correctly
+  (is (= one-event-flattened
+         (events/flatten-two-events one-event))))
